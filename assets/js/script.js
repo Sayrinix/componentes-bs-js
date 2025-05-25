4f6c26981c3208a8bac347297f8556b5d4ed9cc8
Sayrinix <sa.luciano@duocuc.cl>
2025-05-25 19:39:35 -0400

    Actualizacionesss

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,8 @@ document.getElementById('boton').addEventListener('click', function(event){
     //Input type number
     let valorEdad = document.getElementById('edad').value;
     console.log(valorEdad);
+    //Convertir a texto
+    let edad = valorEdad.toString();
     //Input type checkbox
     let valoresComida = document.querySelectorAll('input[name=opciones]:checked');
     let valorSeleccionado = Array.from(valoresComida).map(comida => comida.value);
@@ -16,5 +18,70 @@ document.getElementById('boton').addEventListener('click', function(event){
     //Input type color
     let colorSeleccionado = document.getElementById('color').value;
     console.log(colorSeleccionado);
+    //Input type text (frase favorita)
+    let fraseFav = document.getElementById('frase').value;
+    console.log(fraseFav);
 
+    //Alerta
+    const containerAlerta = document.getElementById('contenedorAlerta')
+    const funcionAlerta = (message) => {
+    const crearCont = document.createElement('div')
+    crearCont.innerHTML = [
+        `<div class="alert alert-primary alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Cerrar alerta"></button>',
+        '</div>'
+    ].join('')
+
+    containerAlerta.append(crearCont)
+    }
+
+    const abrirAlerta = document.getElementById('botonAlerta')
+    if (abrirAlerta) {
+    abrirAlerta.addEventListener('click', () => {
+        funcionAlerta('Tu nombre es: ' + valorNombre);
+    })
+    }
+
+    //Popover
+
+    //Cambiar su contenido
+    let botonPopover = document.getElementById('popoverBtn');
+    botonPopover.setAttribute('data-bs-content', edad);
+
+    //Habilitarlo
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+    
+    //Toast
+
+    //Cambiar su contenido
+    let contenidoToast = document.querySelector('.toast-body');
+    contenidoToast.innerHTML = valorSeleccionado;
+
+    //Habilitarlo
+    const toastTrigger = document.getElementById('toastBtn')
+        const toastLiveExample = document.getElementById('liveToast')
+
+        if (toastTrigger) {
+        const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+        toastTrigger.addEventListener('click', () => {
+            toastBootstrap.show()
+        })
+        }
+
+    //Poner color a offcanvas
+
+    let cuerpo = document.getElementById('cuerpoCanvas');
+    cuerpo.style.backgroundColor = colorSeleccionado;
+
+    //Carrusel
+    let tituloFrase = document.getElementById('fraseFavorita1');
+    tituloFrase.innerHTML = fraseFav;
+
+    let tituloFrase2 = document.getElementById('fraseFavorita2');
+    tituloFrase2.innerHTML = 'Mi nombre es ' + valorNombre;
+
+    let tituloFrase3 = document.getElementById('fraseFavorita3');
+    tituloFrase3.innerHTML = 'Tengo ' + edad + ' años';
 })
